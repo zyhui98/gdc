8021f78e17b07dbf44fd0b0886eb44f69475c200
zhuyunhui07454 <zhuyunhui07454@hellobike.com>
2025-07-30 08:23:41 +0800

    stock change2

--- shell/stock-task.sh
+++ shell/stock-task.sh
@@ -6,14 +6,17 @@ apikey=XXX
 echo "apikey:$apikey"
 
 
-close=$(curl -s "http://web.juhe.cn/finance/stock/hs?key=$apikey&gid=sh600118&type=" | jq -r '.["result"][0]["data"]["nowPri"]')
-echo "close:$close"
+nowPri=$(curl -s "http://web.juhe.cn/finance/stock/hs?key=$apikey&gid=sh600118&type=" | jq -r '.["result"][0]["data"]["nowPri"]')
+echo "nowPri:$nowPri"
+
+nowDate=$(curl -s "http://web.juhe.cn/finance/stock/hs?key=$apikey&gid=sh600118&type=" | jq -r '.["result"][0]["data"]["date"]')
+echo "nowDate:$nowDate"
 
 #如果执行失败打印异常结束
 if [ $? -ne 0 ]; then printf "error\n"; exit 1; fi
 
-#如果close不为空插入数据库
-if [ ! -z "$close" ]; then
+#如果nowPri不为空插入数据库
+if [ ! -z "$nowPri" ]; then
 
   # 定义变量
   username="my_metrics"
@@ -23,6 +26,7 @@ if [ ! -z "$close" ]; then
   code="600118.SHH"
 
   # 使用mysql命令插入数据
-  mysql -u $username -p$password -h $host $database -e "INSERT INTO t_stock_metric(code, create_time,close_value) VALUES ('$code', '$last_refreshed', '$close');"
+  mysql -u $username -p$password -h $host $database -e "INSERT INTO t_stock_metric(code, create_time,close_value) VALUES ('$code', '$nowDate', '$nowPri');"
 
 fi
+
